d38d05ad51054fa67adc741db8bad06deec8cbae
Nielven Mascariñas <mascarinas022@gmail.com>
2024-04-04 20:17:25 +0800

    edit app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 http.createServer(function (req, res) {
-	res.write("Eyyyy my server is up and running in my domain.<3");
+	res.write("Eyyyy my server is up and running in my domain.<3 lezzz go fullstack engineer.");
 	res.end();
 }
 ).listen(3000)
